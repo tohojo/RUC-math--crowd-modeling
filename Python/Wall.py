b87a74ef16a98a162d3b0052991f05ce85951be5
Toke Høiland-Jørgensen <toke@toke.dk>
2010-09-22 14:25:21 +0200

    Fix wall distance calculations when points are past the ends

--- Python/Wall.py
+++ Python/Wall.py
@@ -11,38 +11,45 @@ class Wall:
         self.end = end
 
     def distance_to(self, p):
-        """The distance from the wall to point p
+        """The shortest distance from the wall to point p."""
+        #    From http://www.codeguru.com/forum/showthread.php?t=194400:
+        #
+        #    Let the point be C (Cx,Cy) and the line be AB (Ax,Ay) to (Bx,By).
+        #    Let P be the point of perpendicular projection of C on AB.  The parameter
+        #    r, which indicates P's position along AB, is computed by the dot product 
+        #    of AC and AB divided by the square of the length of AB:
 
-            From http://www.codeguru.com/forum/showthread.php?t=194400:
-        
-            Let the point be C (Cx,Cy) and the line be AB (Ax,Ay) to (Bx,By).
-            Let P be the point of perpendicular projection of C on AB.  The parameter
-            r, which indicates P's position along AB, is computed by the dot product 
-            of AC and AB divided by the square of the length of AB:
+        #        (1)     AC dot AB
+        #            r = ---------  
+        #                ||AB||^2
 
-                (1)     AC dot AB
-                    r = ---------  
-                        ||AB||^2
+        #    r has the following meaning:
 
-            r has the following meaning:
-
-            r=0      P = A
-            r=1      P = B
-            r<0      P is on the backward extension of AB
-            r>1      P is on the forward extension of AB
-            0<r<1    P is interior to AB
+        #    r=0      P = A
+        #    r=1      P = B
+        #    r<0      P is on the backward extension of AB
+        #    r>1      P is on the forward extension of AB
+        #    0<r<1    P is interior to AB
 
 
-            The point P can then be found:
+        #    The point P can then be found:
 
-            Px = Ax + r(Bx-Ax)
-            Py = Ay + r(By-Ay)
+        #    Px = Ax + r(Bx-Ax)
+        #    Py = Ay + r(By-Ay)
 
-            And the distance from A to P = r*L."""
+        #    And the distance from A to P = r*L."""
 
         AC = p-self.start
         AB = self.end-self.start
         r = AC.dot(AB)/self.length()**2
+
+
+        if r > 1:
+            # The point p is past the end of the wall
+            return p.distance_to(self.end)
+        if r < 0:
+            # The point p is past the beginning of the wall
+            return p.distance_to(self.start)
 
         P = Vector(self.start.x + r*(self.end.x-self.start.x),
                 self.start.y + r*(self.end.y-self.start.y))
@@ -54,6 +61,6 @@ class Wall:
         return self.start.distance_to(self.end)
 
 if __name__ == "__main__":
-    p = Vector(0,0)
+    p = Vector(12,-2)
     w = Wall(Vector(-1,-2), Vector(1,-2))
     print w.distance_to(p)
