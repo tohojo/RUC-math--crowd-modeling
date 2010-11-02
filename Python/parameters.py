ddc9c567f47658d14b86d89a3208f9f01744e474
Toke Høiland-Jørgensen <toke@toke.dk>
2010-11-02 23:49:37 +0100

    Add parameters for initial conditions

--- Python/parameters.py
+++ Python/parameters.py
@@ -20,3 +20,16 @@ class actor:
     default_relax_time = 1.0
     default_target = Point(0.0, 10.0)
 
+    initial_number = 10
+    initial_x_range = (-10, 10)
+    initial_y_range = (-10, 10)
+
+    velocity_mean = 1.34
+    velocity_deviation = 0.26
+
+    max_speed = 1.3
+
+    radius_mean = 0.2
+    radius_deviation = 0.05
+
+    target = (0,0)
