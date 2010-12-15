367d2a41989eed3e539385a24136d697dd3fb5e0
Mikkel Hartmann <mikkelheart@gmail.com>
2010-12-15 17:05:09 +0100

    want to pull

--- Python/parameters.py
+++ Python/parameters.py
@@ -5,11 +5,11 @@ from scenario import Scenario
 scenarios = {
         'square_room': Scenario({
             'name'               : 'square_room',
-            'A'                  : 2.2,
+            'A'                  : 8.0,
             'B'                  : 0.2,
-            'U'                  : 2.0,
+            'U'                  : 8.0,
             'lambda'             : 1.0,
-            'initial_count'      : 100,
+            'initial_count'      : 65,
             'start_areas'        : [(-5.0,-5.0,5.0,5.0)],
             'velocity_mean'      : 2.0,
             'velocity_deviation' : 0.26,
@@ -46,8 +46,8 @@ scenarios = {
             'lambda'             : 1.0,
             'initial_count'      : 1,
             'start_areas'        : [
-                                    (-10.0,-1.0,-1.0,1.0),
-                                    (1.0,-1.0,10.0,1.0)
+                                    (-10.0,-0.7,-1.0,0.7),
+                                    (1.0,-0.7,10.0,0.7)
                                    ],
             'velocity_mean'      : 2.0,
             'velocity_deviation' : 0.26,
@@ -62,25 +62,25 @@ scenarios = {
             'flowrate_line'      : (0.0, -1.0, 0.0, 1.0),
             'continuous_rate'    : 10,
             'continuous_start'   : [
-                                    (-10.0, -1.0, -9.0, 1.0),
-                                    (9.0, -1.0, 10.0, 1.0)
+                                    (-10.0, -0.7, -9.0, 0.7),
+                                    (9.0, -0.7, 10.0, 0.7)
                                    ],
-            'stop_at'            : None,
+            'stop_at'            : 50,
             'walls'              : [
-                                    (-10.0,  3.0, 10.0,  3.0),
-                                    (-10.0, -3.0, 10.0, -3.0),
+                                    (-10.0,  1.0, 10.0,  1.0),
+                                    (-10.0, -1.0, 10.0, -1.0)
                                    ],
             'drawing_width'      : 750,
             'drawing_height'     : 350,
             'pixel_factor'       : 30,
             'relax_time'         : 0.1,
             'vary_parameters'    : {
-                                    'A'            : (4.0, 6.0, 0.01),
-                                    #'velocity_mean': (1.0, 5.0, 0.1),
+                                    #'A'            : (4.0, 6.0, 0.01),
+                                    'velocity_mean': (1.5, 5.0, 0.5),
                                    },
         }),
 
-        'widekinks': Scenario({
+        'widekinksbidirec': Scenario({
             'name'               : 'corridor',
             'A'                  : 12.0,
             'B'                  : 0.2,
@@ -92,7 +92,54 @@ scenarios = {
                                     (1.0,-1.0,10.0,1.0)
                                    ],
             'velocity_mean'      : 2.0,
-            'velocity_deviation' : 0.26,
+            'velocity_deviation' : 0.5,
+            'max_velocity_factor': 3.0,
+            'radius_mean'        : 0.2,
+            'radius_deviation'   : 0.01,
+            'targets'            : [
+                                    (500.0,0.0),
+                                    (-500.0, 0.0)
+                                   ],
+            'density_rectangle'  : (-1.0, -0.8, 1.0, 0.8),
+            'flowrate_line'      : (0.0, -1.0, 0.0, 1.0),
+            'continuous_rate'    : 7,
+            'continuous_start'   : [
+                                    (-10.0, -0.7, -9.0, 0.7),
+                                    (9.0, -0.7, 10.0, 0.7)
+                                   ],
+            'stop_at'            : 60,
+            'walls'              : [
+                                    (-10.0,  0.7, -5.0,  0.7),
+                                    (-10.0, -0.7, -5.0, -0.7),
+                                    (-5.0,  0.7, 0.0,  3.0),
+                                    (-5.0, -0.7, 0.0, -3.0),
+                                    (0.0,  3.0, 5.0,  0.7),
+                                    (0.0, -3.0, 5.0, -0.7),
+                                    (5.0,  0.7, 10.0,  0.7),
+                                    (5.0, -0.7, 10.0, -0.7),
+                                   ],
+            'drawing_width'      : 750,
+            'drawing_height'     : 350,
+            'pixel_factor'       : 30,
+            'relax_time'         : 0.1,
+            'vary_parameters'    : {
+                                    #'A'            : (4.0, 6.0, 0.01),
+                                    'velocity_mean': (1.5, 5.0, 0.5),
+                                   },
+        }),
+        'widekinksunidirec': Scenario({
+            'name'               : 'corridor',
+            'A'                  : 12.0,
+            'B'                  : 0.2,
+            'U'                  : 12.0,
+            'lambda'             : 1.0,
+            'initial_count'      : 1,
+            'start_areas'        : [
+                                    (-10.0,-1.0,-1.0,1.0),
+                                    (1.0,-1.0,10.0,1.0)
+                                   ],
+            'velocity_mean'      : 2.0,
+            'velocity_deviation' : 0.5,
             'max_velocity_factor': 3.0,
             'radius_mean'        : 0.2,
             'radius_deviation'   : 0.01,
@@ -104,31 +151,30 @@ scenarios = {
             'flowrate_line'      : (0.0, -1.0, 0.0, 1.0),
             'continuous_rate'    : 10,
             'continuous_start'   : [
-                                    (-10.0, -1.0, -9.0, 1.0),
-                                    (9.0, -1.0, 10.0, 1.0)
+                                    (-10.0, -0.7, -9.0, 0.7)
                                    ],
-            'stop_at'            : None,
+            'stop_at'            : 75,
             'walls'              : [
-                                    (-10.0,  1.0, -5.0,  1.0),
-                                    (-10.0, -1.0, -5.0, -1.0),
-                                    (-5.0,  1.0, 0.0,  3.0),
-                                    (-5.0, -1.0, 0.0, -3.0),
-                                    (0.0,  3.0, 5.0,  1.0),
-                                    (0.0, -3.0, 5.0, -1.0),
-                                    (5.0,  1.0, 10.0,  1.0),
-                                    (5.0, -1.0, 10.0, -1.0),
+                                    (-10.0,  0.7, -5.0,  0.7),
+                                    (-10.0, -0.7, -5.0, -0.7),
+                                    (-5.0,  0.7, 0.0,  3.0),
+                                    (-5.0, -0.7, 0.0, -3.0),
+                                    (0.0,  3.0, 5.0,  0.7),
+                                    (0.0, -3.0, 5.0, -0.7),
+                                    (5.0,  0.7, 10.0,  0.7),
+                                    (5.0, -0.7, 10.0, -0.7),
                                    ],
             'drawing_width'      : 750,
             'drawing_height'     : 350,
             'pixel_factor'       : 30,
             'relax_time'         : 0.1,
             'vary_parameters'    : {
-                                    'A'            : (4.0, 6.0, 0.01),
-                                    #'velocity_mean': (1.0, 5.0, 0.1),
+                                    #'A'            : (4.0, 6.0, 0.01),
+                                    'velocity_mean': (1.5, 5.0, 0.5),
                                    },
         }),
 
-        'bottleneck': Scenario({
+        'bottleneckbidirec': Scenario({
             'name'               : 'corridor',
             'A'                  : 12.0,
             'B'                  : 0.2,
@@ -176,4 +222,50 @@ scenarios = {
                                    },
         }),
 
+        'bottleneckunidirec': Scenario({
+            'name'               : 'corridor',
+            'A'                  : 12.0,
+            'B'                  : 0.2,
+            'U'                  : 12.0,
+            'lambda'             : 1.0,
+            'initial_count'      : 1,
+            'start_areas'        : [
+                                    (-10.0,-1.0,-1.0,1.0),
+                                    (1.0,-1.0,10.0,1.0)
+                                   ],
+            'velocity_mean'      : 2.0,
+            'velocity_deviation' : 0.26,
+            'max_velocity_factor': 3.0,
+            'radius_mean'        : 0.2,
+            'radius_deviation'   : 0.01,
+            'targets'            : [
+                                    (500.0,0.0),
+                                    (-500.0, 0.0)
+                                   ],
+            'density_rectangle'  : (-1.0, -0.8, 1.0, 0.8),
+            'flowrate_line'      : (0.0, -1.0, 0.0, 1.0),
+            'continuous_rate'    : 10,
+            'continuous_start'   : [
+                                    (-10.0, -1.0, -9.0, 1.0),
+                                   ],
+            'stop_at'            : None,
+            'walls'              : [
+                                    (-10.0,  3.0, -5.0,  3.0),
+                                    (-10.0, -3.0, -5.0, -3.0),
+                                    (-5.0,  3.0, 0.0,  1.0),
+                                    (-5.0, -3.0, 0.0, -1.0),
+                                    (0.0,  1.0, 5.0,  3.0),
+                                    (0.0, -1.0, 5.0, -3.0),
+                                    (5.0,  3.0, 10.0,  3.0),
+                                    (5.0, -3.0, 10.0, -3.0),
+                                   ],
+            'drawing_width'      : 750,
+            'drawing_height'     : 350,
+            'pixel_factor'       : 30,
+            'relax_time'         : 0.1,
+            'vary_parameters'    : {
+                                    'A'            : (4.0, 6.0, 0.01),
+                                    #'velocity_mean': (1.0, 5.0, 0.1),
+                                   },
+        }),
 }
