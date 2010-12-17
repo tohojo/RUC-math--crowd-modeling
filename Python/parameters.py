c55fcb18737b895bd425e0eefedf34f82cbc7131
Malik Thrane <ikkens@hotmail.com>
2010-12-17 10:38:05 +0100

    corridor py changed

--- Python/parameters.py
+++ Python/parameters.py
@@ -44,22 +44,22 @@ scenarios = {
             'B'                  : 0.2,
             'U'                  : 2.0,
             'lambda'             : 0.1,
-            'initial_count'      : 50,
+            'initial_count'      : 40,
             'start_areas'        : [
-                                    (-10.0,3.0,10.0,3.0),
-                                    (-10.0,-3.0,10.0,-3.0)
+                                    (-10.0,-3.0,-1.0,3.0),
+                                    (1.0,-3.0,10.0,3.0),
                                    ],
             'velocity_mean'      : 1.34,
             'velocity_deviation' : 0.26,
-            'max_velocity_factor': 1.3,
+            'max_velocity_factor': 4.0,
             'radius_mean'        : 0.3,
             'radius_deviation'   : 0.05,
             'targets'            : [
                                     (500.0,0.0),
                                     (-500.0, 0.0)
                                    ],
-            'density_rectangle'  : (-1.0, -0.8, 1.0, 0.8),
-            'flowrate_line'      : (0.0, -1.0, 0.0, 1.0),
+            'density_rectangle'  : (-1.0, 3.0, 1.0, 3.0),
+            'flowrate_line'      : (-1.0, -3.0, 1.0, -3.0),
             'continuous_rate'    : 3,
             'continuous_start'   : [
                                     (-10.0, -0.7, -9.0, 0.7),
