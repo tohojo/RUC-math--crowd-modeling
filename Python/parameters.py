d0f788fd8a284f2da4ccc627f66083230b57d3c5
Malik Thrane <ikkens@hotmail.com>
2010-12-17 11:39:21 +0100

    Made new plots with correct scaling on the graph. Corrected some in the result, init no. of pedestrians...

--- Python/parameters.py
+++ Python/parameters.py
@@ -51,19 +51,19 @@ scenarios = {
                                    ],
             'velocity_mean'      : 1.34,
             'velocity_deviation' : 0.26,
-            'max_velocity_factor': 4.0,
+            'max_velocity_factor': 1.3,
             'radius_mean'        : 0.3,
             'radius_deviation'   : 0.05,
             'targets'            : [
                                     (500.0,0.0),
                                     (-500.0, 0.0)
                                    ],
-            'density_rectangle'  : (-1.0, 3.0, 1.0, 3.0),
-            'flowrate_line'      : (-1.0, -3.0, 1.0, -3.0),
+            'density_rectangle'  : (-1.0, -3.0, 1.0, 3.0),
+            'flowrate_line'      : (0.0, -3.0, 0.0, 3.0),
             'continuous_rate'    : 3,
             'continuous_start'   : [
-                                    (-10.0, -0.7, -9.0, 0.7),
-                                    (9.0, -0.7, 10.0, 0.7)
+                                    (-10.0, -3.0, -9.0, 3.0),
+                                    (9.0, -3.0, 10.0, 3.0),
                                    ],
             'stop_at'            : 60,
             'walls'              : [
@@ -73,7 +73,7 @@ scenarios = {
             'drawing_width'      : 750,
             'drawing_height'     : 350,
             'pixel_factor'       : 30,
-            'relax_time'         : 1.0,
+            'relax_time'         : 0.5,
             'vary_parameters'    : {
                                     #'A'            : (4.0, 6.0, 0.01),
                                     'velocity_mean': (1.5, 5.0, 0.5),
@@ -147,13 +147,14 @@ scenarios = {
                                     (500.0,0.0),
                                     (-500.0, 0.0)
                                    ],
-            'density_rectangle'  : (-1.0, -0.8, 1.0, 0.8),
+            'density_rectangle'  : (-1.0, -3.0, 1.0, 3.0),
             'flowrate_line'      : (0.0, -1.0, 0.0, 1.0),
-            'continuous_rate'    : 10,
+            'continuous_rate'    : 3,
             'continuous_start'   : [
-                                    (-10.0, -0.7, -9.0, 0.7)
+                                    (-10.0,-3.0,-9.0,3.0),
+                                    (9.0,-3.0,10.0,3.0),
                                    ],
-            'stop_at'            : 75,
+            'stop_at'            : 60,
             'walls'              : [
                                     (-10.0,  0.7, -5.0,  0.7),
                                     (-10.0, -0.7, -5.0, -0.7),
