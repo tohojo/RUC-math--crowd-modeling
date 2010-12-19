ffee7cfbc6e38f573811fcbdb5c5222974064870
Mikkel Hartmann <mikkelheart@gmail.com>
2010-12-19 16:17:31 +0100

    my paramaters

--- Python/parameters.py
+++ Python/parameters.py
@@ -45,7 +45,7 @@ scenarios = {
             'name'               : 'corridor',
             'A'                  : 3.0,
             'B'                  : 0.2,
-            'U'                  : 2.0,
+            'U'                  : 5.0,
             'lambda'             : 1.0,
             'initial_count'      : 1,
             'start_areas'        : [
@@ -55,23 +55,26 @@ scenarios = {
             'velocity_mean'      : 2.0,
             'velocity_deviation' : 0.26,
             'max_velocity_factor': 1.3,
-            'radius_mean'        : 0.3,
-            'radius_deviation'   : 0.02,
+            'radius_mean'        : 0.2,
+            'radius_deviation'   : 0.001,
             'targets'            : [
                                     (500.0,0.0),
                                     (-500.0, 0.0)
                                    ],
             'density_rectangle'  : (-1.0, -.0, 1.0, 3.0),
-            'flowrate_line'      : (0.0, -3.0, 0.0, 3.0),
+            'flowrate_lines'      :[
+                                    (-11.0, -1.0, -11.0, 1.0),
+                                    (11.0, -1.0, 11.0, 1.0)
+                                  ],
             'continuous_rate'    : 5,
             'continuous_start'   : [
                                     (-10.0, -1.0, -8.0, 1.0),
-                                    (8.0, -1.0, 10.0, 1.0),
+                                    (8.0, -1.0, 10.0, 1.0)
                                    ],
-            'stop_at'            : 50,
+            'stop_at'            : 300,
             'walls'              : [
-                                    (-10.0,  2.5, 10.0,  2.5),
-                                    (-10.0, -2.5, 10.0, -2.5)
+                                    (-10.0,  1.0, 10.0,  1.0),
+                                    (-10.0, -1.0, 10.0, -1.0)
                                    ],
             'drawing_width'      : 750,
             'drawing_height'     : 350,
